17705dcd8f4dd5ce27bb2d1f7c8b3fa4c1bc1e1f
JefeLitman <jefelitman@gmail.com>
2022-09-14 20:36:36 +0000

    oTree Skel Sections

---
+++ otree_server/consentimiento/__init__.py
@@ -0,0 +1,43 @@
+"""File containing the consent pages for the players
+Version: 0.1
+Made By: Edgar RP
+"""
+from otree.api import *
+
+doc = """
+Your app description
+"""
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'consentimiento'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    pass
+
+
+# PAGES
+class MyPage(Page):
+    pass
+
+
+class ResultsWaitPage(WaitPage):
+    pass
+
+
+class Results(Page):
+    pass
+
+
+page_sequence = [MyPage, ResultsWaitPage, Results]
