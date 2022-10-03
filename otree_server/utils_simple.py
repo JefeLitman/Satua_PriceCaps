d71ed0ad78e1ec798e1c5501c212d8bb4435e2b6
Edgar RP <jefelitman@gmail.com>
2022-10-03 06:22:54 +0000

    oTree utils_simple v0.2

--- otree_server/utils_simple.py
+++ otree_server/utils_simple.py
@@ -1,5 +1,5 @@
 """File containing utilities functions for every simple section in the whole app
-Version: 0.1
+Version: 0.2
 Made By: Edgar RP
 """
 
@@ -14,37 +14,41 @@ def set_group_asks_bids(group):
     for i, p in enumerate(group.get_players()):
         p.bid_value = bids_randomized[i]
     
-def get_price(group):
-    asks = sorted([int(i) for i in group.session.config["seller_asks"].split(",")])
-    bids = []
-    for player in group.get_players():
-        participated = player.field_maybe_none("enter_bid")
+def get_active_players_market(player):
+    bids = [(player.id_in_group, player.bid_value)]
+    for p in player.get_others_in_group():
+        participated = p.field_maybe_none("enter_bid")
         if participated:
-            bids.append(player.bid_value)
-    bids = sorted(bids, reverse=True)
-    max_ask = max(asks) if group.session.config["treatment_FMI"] else 7
-    for i in range(len(bids) - 1, -1, -1):
-        if bids[i] >= asks[i]:
-            return asks[i] if asks[i] <= max_ask else max_ask
-    return max_ask
+            bids.append((p.id_in_group, p.bid_value))
+    sorted_bids = sorted(bids, reverse=True, key=lambda x: x[1])
+    return sorted_bids
 
-def player_bid(player, value, section):
-    player.bid_value = int(value)
-    price = get_price(player.group)
-    accepted = int(value) >= price
-    if section == player.participant.winner_section and player.round_number == player.participant.winner_round:
-        player.payoff = player.max_value - price if accepted else 0
-    return {
-        "bid": value, 
-        "accepted": accepted,
-        "price": price
-    }
-
-def add_history_value(player, value):
-    history = player.field_maybe_none("bid_history")
-    if history == None:
-        values = []
+def player_bid(player, entered, section):
+    player.enter_bid = int(entered)
+    earnings = 0
+    player.payoff = 0
+    if player.enter_bid:
+        asks = sorted([int(i) for i in player.session.config["seller_asks"].split(",")])
+        max_ask = max(asks) if player.session.config["treatment_FMI"] else 7
+        bids = get_active_players_market(player)
+        table_results = [bid + (asks[i], bid[1] >= asks[i]) for i, bid in enumerate(bids)]
+        price = table_results[-1][2] if table_results[-1][2] <= max_ask else max_ask
+        for p_id, p_bid, s_ask, traded in table_results:
+            if p_id == player.id_in_group:
+                if traded and s_ask <= price:
+                    accepted = "Si"
+                    earnings = p_bid - price
+                    if section == player.participant.winner_section and player.round_number == player.participant.winner_round:
+                        player.payoff = p_bid - price 
+                else:
+                    accepted = "No"
+                break
     else:
-        values = history.split("-")
-    values.append(value)
-    player.bid_history = "-".join([str(i) for i in values])
+        accepted = "No esta participando"
+        price = "No esta participando"
+
+    return {
+        "accepted": accepted,
+        "price": price,
+        "earnings": earnings
+    }
