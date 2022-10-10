1a9865256830bca941f1e7bb8aaa1a2207199198
JefeLitman <jefelitman@gmail.com>
2022-10-10 23:20:42 +0000

    oTree seccion_5 config v1.0

--- otree_server/seccion_5/__init__.py
+++ otree_server/seccion_5/__init__.py
@@ -1,40 +1,61 @@
+"""File containing the section 5 configuration of players
+Version: 1.0
+Made By: Edgar RP
+"""
 from otree.api import *
-
+import random
+from utils_simple import set_experiment_params
 
 doc = """
 Your app description
 """
-
 
 class C(BaseConstants):
     NAME_IN_URL = 'seccion_5'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
 
-
 class Subsession(BaseSubsession):
     pass
-
 
 class Group(BaseGroup):
     pass
 
-
 class Player(BasePlayer):
-    pass
-
+    winner_round = models.StringField()
+    winner_section = models.StringField()
+    treatment = models.StringField()
+    ticket_color = models.StringField()
+    lotery_selection = models.IntegerField()
 
 # PAGES
-class MyPage(Page):
-    pass
+class wait_for_all_grouping(WaitPage):
+    wait_for_all_groups = True
+    @staticmethod
+    def after_all_players_arrive(subsession):
+        players = subsession.get_players()
+        n_players = len(players)
+        randomized_players = random.sample(players, n_players)
+        for i, p in enumerate(randomized_players):
+            if i < n_players // 2:
+                p.ticket_color = "Roja"
+            else:
+                p.ticket_color = "Azul"
+            set_experiment_params(p)
+    
+    @staticmethod
+    def is_displayed(player):
+        return player.participant.consentimiento
 
+class O001_loteria(Page):
+    form_model = 'player'
+    form_fields = ['lotery_selection']
 
-class ResultsWaitPage(WaitPage):
-    pass
+    @staticmethod
+    def is_displayed(player):
+        return player.participant.consentimiento
 
-
-class Results(Page):
-    pass
-
-
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [
+    wait_for_all_grouping,
+    O001_loteria
+]
