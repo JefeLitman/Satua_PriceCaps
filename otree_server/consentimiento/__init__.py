b03db1a71ef3c71a81ca8984a2d270e4fc76a753
Edgar RP <jefelitman@gmail.com>
2022-10-11 05:09:20 +0000

    oTree consentimiento config v1.6

--- otree_server/consentimiento/__init__.py
+++ otree_server/consentimiento/__init__.py
@@ -1,5 +1,5 @@
 """File containing the consent pages for the players
-Version: 1.5
+Version: 1.6
 Made By: Edgar RP
 """
 import random
@@ -13,7 +13,7 @@ Your app description
 
 class C(BaseConstants):
     NAME_IN_URL = 'consentimiento'
-    PLAYERS_PER_GROUP = 8
+    PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
 
 
@@ -31,6 +31,23 @@ class Player(BasePlayer):
     winner_section = models.StringField()
     treatment = models.StringField()
 
+def creating_session(subsession):
+    if (len(subsession.get_players()) / 4) % 2 != 0:
+        raise AssertionError("The quantity of players must have an even number of groups with 4 player per group.")
+    random.seed(subsession.session.config["seed"]) #Implement the seed to replicate when creating sessions
+    if subsession.session.config["treatment_FMI"]:
+        sections = [1, 3, 4, 5, 6]
+    else:
+        sections = [1, 2, 3, 4, 5, 6]
+    # if player.session.config["name"].lower() == "Precios_Maximos_Full".lower():
+    #     player.participant.winner_round = random.choice(range(1, 41))
+    # else:
+    subsession.session.winner_section = random.choice(sections)
+    if subsession.session.winner_section < 5:
+        subsession.session.winner_round = random.choice(range(1, 9))
+    else:
+        subsession.session.winner_round = 1
+
 # PAGES
 class O001_consent(Page):
     form_model = 'player'
@@ -39,16 +56,6 @@ class O001_consent(Page):
     @staticmethod
     def before_next_page(player, timeout_happened):
         player.participant.consentimiento = player.accepted
-        random.seed(player.session.config["seed"]) #Implement the seed to replicate when creating sessions
-        if player.session.config["treatment_FMI"]:
-            sections = [1, 3, 4, 5, 6]
-        else:
-            sections = [1, 2, 3, 4, 5, 6]
-        if player.session.config["name"].lower() == "Precios_Maximos_Full".lower():
-            player.participant.winner_round = random.choice(range(1, 41))
-        else:
-            player.participant.winner_round = random.choice(range(1, 9))
-        player.participant.winner_section = random.choice(sections)
         set_experiment_params(player)
 
 page_sequence = [O001_consent]
