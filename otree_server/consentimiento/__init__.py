1d25a0e12b233214e4437fd9065175a0b475da45
Edgar RP <jefelitman@gmail.com>
2022-10-17 14:46:53 +0000

    oTree consentimiento config v1.7

--- otree_server/consentimiento/__init__.py
+++ otree_server/consentimiento/__init__.py
@@ -1,9 +1,9 @@
 """File containing the consent pages for the players
-Version: 1.6
+Version: 1.7
 Made By: Edgar RP
 """
 import random
-from utils_simple import set_experiment_params
+from utils import set_experiment_params
 from otree.api import *
 
 doc = """
@@ -34,14 +34,16 @@ class Player(BasePlayer):
 def creating_session(subsession):
     if (len(subsession.get_players()) / 4) % 2 != 0:
         raise AssertionError("The quantity of players must have an even number of groups with 4 player per group.")
+    treatments = (subsession.session.config["treatment_FME"], subsession.session.config["treatment_FMI"], subsession.session.config["treatment_PCE"])
+    if sum(treatments) != 1:
+        raise AssertionError("You need to chose only one treatment for the session to start.")
+    if len(subsession.session.config["seller_asks"].split(",")) != len(subsession.session.config["players_bids"].split(",")) != 4:
+        raise AssertionError("The quantity of seller asks and player bids must be equal to 4 each.")
     random.seed(subsession.session.config["seed"]) #Implement the seed to replicate when creating sessions
     if subsession.session.config["treatment_FMI"]:
         sections = [1, 3, 4, 5, 6]
     else:
         sections = [1, 2, 3, 4, 5, 6]
-    # if player.session.config["name"].lower() == "Precios_Maximos_Full".lower():
-    #     player.participant.winner_round = random.choice(range(1, 41))
-    # else:
     subsession.session.winner_section = random.choice(sections)
     if subsession.session.winner_section < 5:
         subsession.session.winner_round = random.choice(range(1, 9))
@@ -56,6 +58,7 @@ class O001_consent(Page):
     @staticmethod
     def before_next_page(player, timeout_happened):
         player.participant.consentimiento = player.accepted
+        player.participant.section_setting = None
         set_experiment_params(player)
 
 page_sequence = [O001_consent]
