9837f9764bad148ea20e05ef4ba4bde2328ce2e7
Edgar RP <jefelitman@gmail.com>
2022-11-17 04:32:31 +0000

    oTree informacion config v1.6

--- otree_server/informacion/__init__.py
+++ otree_server/informacion/__init__.py
@@ -1,5 +1,5 @@
 """File containing the general and payments information for players
-Version: 1.5
+Version: 1.6
 Made By: Edgar RP
 """
 
@@ -32,18 +32,36 @@ class Player(BasePlayer):
     treatment = models.StringField()
 
 def creating_session(subsession):
+    if subsession.session.config["name"] == "Lottery_First":
+        subsession.session.lottery_section_number = 1
+        subsession.session.assignation_section_number = 2
+        subsession.session.market_initial_number = 3
+        if subsession.session.config["treatment_FMI"]:
+            sections = [1, 2, 3, 5, 6]
+        else:
+            sections = [1, 2, 3, 4, 5, 6]
+    elif subsession.session.config["name"] == "Market_First":
+        subsession.session.lottery_section_number = 5
+        subsession.session.assignation_section_number = 6
+        subsession.session.market_initial_number = 1
+        if subsession.session.config["treatment_FMI"]:
+            sections = [1, 3, 4, 5, 6]
+        else:
+            sections = [1, 2, 3, 4, 5, 6]
+    else:
+        raise AssertionError("You tried to create an invalid session without any basic configuration set.")
     treatments = (subsession.session.config["treatment_FME"], subsession.session.config["treatment_FMI"], subsession.session.config["treatment_PCE"])
     if sum(treatments) != 1:
         raise AssertionError("You need to chose only one treatment for the session to start.")
     if len(subsession.session.config["seller_asks"].split(",")) != len(subsession.session.config["players_bids"].split(",")) != 4:
         raise AssertionError("The quantity of seller asks and player bids must be equal to 4 each.")
     random.seed(subsession.session.config["seed"]) #Implement the seed to replicate when creating sessions
-    if subsession.session.config["treatment_FMI"]:
-        sections = [1, 3, 4, 5, 6]
-    else:
-        sections = [1, 2, 3, 4, 5, 6]
     subsession.session.winner_section = random.choice(sections)
-    if subsession.session.winner_section < 5:
+    excluded_rounds = [
+            subsession.session.lottery_section_number, 
+            subsession.session.assignation_section_number
+        ]
+    if subsession.session.winner_section not in excluded_rounds:
         subsession.session.winner_round = random.choice(range(1, 9))
     else:
         subsession.session.winner_round = 1
